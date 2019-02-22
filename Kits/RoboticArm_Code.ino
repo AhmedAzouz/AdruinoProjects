dc60b6a6b289387f07c0bfdae94c33d077078ffa
AhmedAzouz <ahmedfaragazouz@gmail.com>
2019-02-22 07:59:01 +0300

    Add files via upload

---
+++ Kits/RoboticArm_Code.ino
@@ -0,0 +1,149 @@
+// ***********************************
+// Robotic Arm Kit v1.0
+// San3aHub.com by AhmedFarag
+// ***********************************
+
+#include Servo MyServo,MyServo2,MyServo3,MyServo0;
+char ByteReceived ;
+int MyPosition,MyPosition2,MyPosition3,MyPositionG;
+
+int runCounter = 0;
+int ledPin = A5;// set the default home servo positions 
+int valGripper = 30;
+int valBase = 70;
+int valShoulder = 25;
+int valElbow = 150;
+
+void setup()
+{
+ Serial.begin(9600); 
+ 
+ MyServo.attach(6); // base  attach
+ MyServo2.attach(9); //shoulder  attach
+ MyServo3.attach(10); //Elbow attach
+ MyServo0.attach(11); // Gripper  attach
+ 
+ delay(15);
+ MyServo.write(valBase);   // base
+ delay(30);
+ MyServo2.write(valShoulder);  // shoulder
+ delay(30);
+ MyServo3.write(valElbow); // elbow
+ delay(300);
+}
+ 
+void loop()
+{    
+    
+    ByteReceived = Serial.read();  switch (ByteReceived) {    // ************* LED
+      
+      break;    // ************* Base
+      case 'l':
+      MyPosition = MyServo.read() + 2;    // L for move base left
+      MyServo.write(MyPosition);      Serial.print("Base");
+      Serial.print("\t");
+      Serial.println(MyServo.read()); 
+      delay(15);
+      
+      break;    
+      
+      case 'r':
+      MyPosition = MyServo.read() - 2;    // R for move base right
+      MyServo.write(MyPosition);
+      
+      Serial.print("Base");
+      Serial.print("\t");
+      Serial.println( MyServo.read()); 
+      delay(15);
+      
+      break;
+      
+      // ************* Elbow
+      case 'u':
+      MyPosition2 = MyServo2.read() + 2;   // U for move Elbow up
+      MyServo2.write(MyPosition2);      Serial.print("Elbow");
+      Serial.print("\t");
+      Serial.println(MyServo2.read()); 
+      delay(15);
+      
+      break;      case 'd':
+      MyPosition2 = MyServo2.read() - 2;   // D for move Elbow down
+      MyServo2.write(MyPosition2);      Serial.print("Elbow");
+      Serial.print("\t");
+      Serial.println(MyServo2.read()); 
+      delay(15);
+      
+      break;      // ************* Shoulder
+      case 'w':
+      MyPosition3 = MyServo3.read() + 2;  // W for move Shoulder down
+      MyServo3.write(MyPosition3);      Serial.print("Shoulder");
+      Serial.print("\t");
+      Serial.println(MyServo3.read()); 
+      delay(15);
+      
+      break;      case 's':
+      MyPosition3 = MyServo3.read() - 2;  // S for move Shoulder up
+      MyServo3.write(MyPosition3);      Serial.print("Shoulder");
+      Serial.print("\t");
+      Serial.println(MyServo3.read()); 
+      delay(15);
+      
+      break;      // ************* Gripper
+      case 'o':
+      MyServo0.write(30);                // O to open Gripper
+      Serial.print("Gripper Open");
+      Serial.print("\t");
+      Serial.println(MyServo0.read()); 
+      delay(15);
+      
+      break;      case 'c':                          // C to open Gripper
+      MyServo0.write(1);
+      Serial.print("Gripper Close");
+      Serial.print("\t");
+      Serial.println(MyServo0.read()); 
+      delay(15);
+      
+      break;      // ************* Default Dropped position (quick drop position)
+      case 'q':
+      
+       digitalWrite(ledPin,HIGH); 
+       Serial.println("Default Dropped activated!");  
+       Pick();
+       Drop();
+       Home();
+       digitalWrite(ledPin,LOW);
+      
+      break;      
+      // ************* Force Stop and go home position
+      case 'h':
+      
+       Home();
+       digitalWrite(ledPin,LOW);
+      
+      break; 
+
+      // ************* print position
+      case 'p':
+      
+       positions();
+       digitalWrite(ledPin,LOW);
+      break; 
+
+      // ************* print position
+      case 'z':
+      
+       Serial.println("Robotic Arm Ready!");
+       break; 
+      
+      // ************* Automatic mood runs Playback function
+       // serial read Must be in this format: 1:90&2:80&3:180
+       // pickup place is fixed, this function will automate the drop position.
+      default:         digitalWrite(ledPin,HIGH); 
+         Serial.println("Automatic mood activated!");  
+         Pick();
+         Playback();
+         Home();
+         digitalWrite(ledPin,LOW);  
+      
+      break;
+  }}}
---
+++ Kits/Tasks.ino
@@ -0,0 +1,112 @@
+int delayT = 350;
+void positions()
+{
+    Serial.print("Base : ");
+    Serial.print(valBase);
+    Serial.print("\t");
+    Serial.print("Shou : ");
+    Serial.print(valShoulder);
+    Serial.print("\t");
+    Serial.print("Elbow : ");
+    Serial.print(valElbow);
+    Serial.print("\t");
+    Serial.print("Grip : ");
+    Serial.println(valGripper);
+}
+}
+void Home() //Call this fucntion when u want to set arm in home position
+  {
+    MyServo0.write(valGripper);   // Gripper
+    delay(15);
+    MyServo.write(valBase);   // base
+    delay(30);
+    MyServo2.write(valShoulder);  // shoulder
+    delay(30);
+    MyServo3.write(valElbow); // elbow
+    delay(delayT);
+  }
+  
+  void Pick() // This is fixed pick place.
+  {
+   MyServo.write(4);   // base
+   delay(delayT);
+  
+   MyServo3.write(125); // elbow 
+   MyServo2.write(95);  // shoulder
+   delay(delayT);
+   
+   MyServo0.write(50);   // Gripper open wide
+   delay(delayT);
+   
+   MyServo0.write(2);   // Gripper close
+   delay(delayT);
+  
+   MyServo2.write(60);  // shoulder up little
+   MyServo3.write(80);  // elbow up little
+   delay(delayT);
+   
+   Serial.println("Object Pickeded");
+  }
+  
+  void Drop() // This is fixed Drop place.
+  { 
+   MyServo.write(145);   // base
+   delay(delayT);
+   
+   MyServo2.write(80);  // shoulder
+   delay(15);
+   
+   MyServo3.write(115); // elbow
+   delay(delayT);
+  
+   MyServo0.write(40);   // Gripper open wide
+   delay(delayT);
+  
+   MyServo3.write(90);  // elbow up little
+   delay(delayT);
+   
+   Serial.println(F("Object Dropped"));
+   }
+   
+   void  Playback()
+  {
+        
+    //      String phrase;
+    //      phrase = String(phrase + ByteReceived); // convert the char input to stirng can be split    // Read each command pair 
+    char* command = strtok(ByteReceived, "&");
+    while (command != 0)
+    {
+        // Split the command in two values
+        char* separator = strchr(command, ':');
+        if (separator != 0)
+        {
+            // Actually split the string in 2: replace ':' with 0
+            *separator = 0;
+            int servoId = atoi(command);
+            ++separator;
+            int angle = atoi(separator);
+    
+            // Do something with servoId and angle
+            
+            if (servoId = 1)
+            {
+               MyServo.write(angle);
+                delay(delayT);
+            }
+            else if (servoId = 2)
+            {
+               MyServo2.write(angle);
+                delay(delayT);
+            }
+            else if (servoId = 3)
+            {
+              MyServo3.write(angle);
+                delay(delayT);
+            }
+            
+        }
+        // Find the next command in input string
+        command = strtok(0, "&");
+    }
+    
+  }
